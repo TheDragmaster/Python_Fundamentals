a0e705ee26c319bee41ca4eddd218245c7d13f23
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2023-08-20 09:54:35 -0500

    Add files via upload

---
+++ if_Statements.py
@@ -0,0 +1,15 @@
+n = 1
+if n > 2:
+    n -= 1
+elif n == 2:
+    n *= 2
+else:
+    n += 2
+
+    # and = &&
+    # or = ||
+    #Parentheses are only used in multi line conditions
+x, y = 1, 2
+if ((x > 2 and
+     y != y) or n == y):
+    x += y 
