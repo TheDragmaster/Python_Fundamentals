2667ef24cb2bc7cf9967c232dafd260f6f69227b
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2023-08-21 23:23:53 -0500

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,12 @@
+
+#Just a for loop, nothing special
+for i in range(5):
+    print(i)
+
+# Loop within set range
+for n in range(2, 6):
+    print (n)
+
+#Loop in reverse order 
+for r in range(5, 1, -1):
+    print (r)
