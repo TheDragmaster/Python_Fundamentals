0fafa650759c5ec6e02d3dde1209d40b754964ef
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2023-08-27 20:35:54 -0500

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,15 @@
+# Strings are similar to arrays 
+s = "abc"
+print(s[0:2])   #Prints only a and b 
+
+
+#So this creates a new string 
+s += "def"
+print(s)
+
+# Valid numeric strings can be converted 
+print(int("123")   + int("123"))
+
+# And numbers can be converted to strings
+print(str(123) + str(123))
+
