94fda38a9668912c11cbd3d53cf391abc0a22e18
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2023-08-29 11:12:56 -0500

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -13,3 +13,6 @@ print(int("123")   + int("123"))
 # And numbers can be converted to strings
 print(str(123) + str(123))
 
+#Combine a list of strings(with an empty string delimitor)
+strings = ["ab", "cd", "ef"]
+print("".join(strings))
